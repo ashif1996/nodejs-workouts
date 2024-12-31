3a0c3589d02ca4f8c7a0060d322fe0934731b010
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-31 20:58:37 +0530

    create a simple HTTP server that responds with 'Hello, World!'

---
+++ workouts/httpServer.js
@@ -0,0 +1,13 @@
+// Create a simple HTTP server that responds with "Hello, World!".
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
